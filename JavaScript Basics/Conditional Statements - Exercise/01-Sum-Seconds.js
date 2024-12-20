f2811c6a2c9cda5cf5b307f2f2f67aa7a0b5d0fb
Yavor Totev <yavortotev@yahoo.com>
2024-12-20 17:14:57 -0600

    Add 01-Sum-Seconds.js

---
+++ JavaScript Basics/Conditional Statements - Exercise/01-Sum-Seconds.js
@@ -0,0 +1,22 @@
+function sumSeconds(input){
+
+    let timeFirst = Number(input[0]);
+    let timeSecind = Number(input[1]);
+    let timeThird = Number(input[2]);
+
+    let totalTime = timeFirst + timeSecind + timeThird;
+
+    let minutes = Math.floor(totalTime / 60);
+    let seconds = totalTime % 60;
+
+    if (seconds < 10){
+
+        console.log(`${minutes}:0${seconds}`)
+
+    }else{
+        console.log(`${minutes}:${seconds}`)
+    }
+
+}
+
+sumSeconds(['12','12','10'])
