fe190694471dd811126cfa893a2a94f66c11acac
Yavor Totev <yavortotev@yahoo.com>
2024-12-20 12:30:51 -0600

    Add 02-Greater-Number.js

---
+++ 2.Conditional-Steatment-Exercises/02-Greater-Number.js
@@ -0,0 +1,19 @@
+function greaterNumber(input){
+
+    let  num1 = Number(input[0]);
+    let num2 = Number(input[1]);
+
+    if (num1 > num2){
+        console.log(num1)
+    }else{
+        console.log(num2)
+    }
+
+
+     //ternary operator: 
+    //console.log( num1>num2 ? num1 : num2);
+    
+
+}
+
+greaterNumber(['10','11'])
