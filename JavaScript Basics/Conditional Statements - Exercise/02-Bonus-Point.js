ddcc25607d934f80abb7f2577f8b187a05569aa0
Yavor Totev <yavortotev@yahoo.com>
2024-12-20 17:20:10 -0600

    Add 02-bonus-point.js

---
+++ JavaScript Basics/Conditional Statements - Exercise/02-Bonus-Point.js
@@ -0,0 +1,22 @@
+function bonusPoints(input) {
+    let points = Number(input[0]);
+    let bonus = 0;
+
+    if (points <= 100) {
+        bonus = 5;
+    } else if (points <= 1000) {
+        bonus = 0.2 * points;
+    } else {
+        bonus = 0.1 * points;
+    }
+
+    if (points % 2 === 0) {
+        bonus = bonus + 1;
+    } else if (points % 10 === 5) {
+        bonus = bonus + 2;
+    }
+    console.log(bonus);
+    console.log(points + bonus);
+}
+
+bonusPoints(['20']);
