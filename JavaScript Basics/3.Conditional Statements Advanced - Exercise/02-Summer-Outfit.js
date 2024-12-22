2843d23e24c995e27d1dcc7d30874632d8ca45a1
Yavor Totev <yavortotev@yahoo.com>
2024-12-21 18:29:52 -0600

    Add 02-Summer-Outfit.js

---
+++ JavaScript Basics/3.Conditional Statements Advanced - Exercise/02-Summer-Outfit.js
@@ -0,0 +1,60 @@
+function summerOutfit(input) {
+
+    let degrees = Number(input[0]);
+    let timeDuringTheDay = input[1];
+
+    let outfit = " ";
+    let shoes = " ";
+
+    switch (timeDuringTheDay) {
+        case 'Morning':
+            if (degrees >= 10 && degrees <= 18) {
+                outfit = 'Sweatshirt';
+                shoes = 'Sneakers';
+                console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+            } else if (degrees > 18 && degrees <= 24) {
+                outfit = 'Shirt';
+                shoes = 'Moccasins';
+                console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+            } else if (degrees >= 25) {
+                outfit = 'T-Shirt';
+                shoes = 'Sandals';
+                console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+            }
+            break;
+
+        case 'Afternoon':
+            if (degrees >= 10 && degrees <= 18) {
+                outfit = 'Shirt';
+                shoes = 'Moccasins';
+                console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+            } else if (degrees > 18 && degrees <= 24) {
+                outfit = 'T-Shirt';
+                shoes = 'Sandals';
+                console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+            } else if (degrees >= 25) {
+                outfit = 'Swim Suit';
+                shoes = 'Barefoot';
+                console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+            }
+            break;
+
+        case 'Evening':
+            if (degrees >= 10 && degrees <= 18) {
+                outfit = 'Shirt';
+                shoes = 'Moccasins';
+                console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+            } else if (degrees > 18 && degrees <= 24) {
+                outfit = 'Shirt';
+                shoes = 'Moccasins';
+                console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+            } else if (degrees >= 25) {
+                outfit = 'Shirt';
+                shoes = 'Moccasins';
+                console.log(`It's ${degrees} degrees, get your ${outfit} and ${shoes}.`);
+            }
+            break;
+    }
+}
+
+summerOutfit(["22", "Afternoon"]);
