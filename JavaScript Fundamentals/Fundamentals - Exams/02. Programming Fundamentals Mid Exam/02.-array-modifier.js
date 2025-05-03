d3a6a454734fdcf4dba79fb6679a09cef15326b1
Yavor Totev <yavortotev@yahoo.com>
2025-05-03 15:31:16 -0500

    add 02.-array-modifier.js

--- JavaScript Fundamentals/Fundamentals - Exams/02. Programming Fundamentals Mid Exam/02.-array-modifier.js
+++ JavaScript Fundamentals/Fundamentals - Exams/02. Programming Fundamentals Mid Exam/02.-array-modifier.js
@@ -23,3 +23,14 @@ function solve(commands) {
 
     console.log(numbers.join(', '));
 }
+
+
+solve( [
+    '23 -2 321 87 42 90 -123',
+    'swap 1 3',
+    'swap 3 6',
+    'swap 1 0',
+    'multiply 1 2',
+    'multiply 2 1',
+    'decrease',
+    'end'])
